eab6941cded1decb926e98519bf6e009f05ca256
Daniel A.García <dnlrsgrc@gmail.com>
2025-11-12 13:38:28 +0100

    feat:ejercicio63

--- src/App.java
+++ src/App.java
@@ -2,11 +2,11 @@ public class App {
     public static String pintaPiramide(int altura, int fila) {
         String resultado = "";
         if (fila >= 1) {
-            // Espacios antes de la pirámide
+           
             for (int i = 1; i <= altura - fila; i++) {
                 resultado += " ";
             }
-            // Asteriscos de la pirámide
+      
             for (int j = 1; j <= 2 * fila - 1; j++) {
                 resultado += "*";
             }
@@ -28,20 +28,19 @@ public class App {
         for (int i = 1; i <= alt1; i++) {
             String linea = "";
 
-            // Primera pirámide
             linea += pintaPiramide(alt1, i);
 
-            // Espacios entre pirámides
+           
             linea += pintaEspacios(espacios);
 
-            // Segunda pirámide: alineada y recta
+           
             if (i <= alt1 - alt2) {
-                // Espacios para mantener hueco
+              
                 linea += pintaEspacios(alt2 * 2 - 1);
             } else {
-                // Fila relativa dentro de la pirámide pequeña
+                
                 int filaRelativa = i - (alt1 - alt2);
-                // Aquí usamos alt2 para centrarla con SU altura
+                
                 linea += pintaPiramide(alt2, filaRelativa);
             }
 
